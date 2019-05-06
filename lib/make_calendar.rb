1c0f711702eb315766aad35d15d8b90cfacdb9fd
keisuke713 <nebakei.tkb713@gmail.com>
2019-05-06 14:14:51 +0900

    refactoring

--- lib/make_calendar.rb
+++ lib/make_calendar.rb
@@ -25,40 +25,26 @@ WEEK_DAY = {
 }
 
 def make_calendar(year,month)
-  start_day = Date.new(year,month,1)
+  first_day = Date.new(year,month,1)
+  second_day = Date.new(year,month,2)
   end_day = Date.new(year,month,-1)
 
   puts "\t#{TRANSLATE_MONTH[month]} #{year}"
-  
+
   WEEK_DAY.each do |key,value|
-    if Sat?(key)
-      puts value
-    else
-      print value
-    end
+    print value
+    puts if sat?(key)
   end
 
-  (start_day..end_day).each do |d|
-    if d.day == 1
-      if Sat?(d.wday)
-        puts "#{d.day.to_s.rjust(space(d.wday))} "
-      else
-        print "#{d.day.to_s.rjust(space(d.wday))} "
-      end
+  print "#{'1'.rjust(space(first_day.wday))} "
+  puts if sat?(first_day.wday)
+
+  (second_day..end_day).each do |d|
+    print "%3d" % d.day
+    if sat?(d.wday)
+      puts
     else
-      if d.day >= 10
-        if Sat?(d.wday)
-          puts one_space(d.day)
-        else
-          print one_space(d.day)
-        end
-      else
-        if Sat?(d.wday)
-          puts two_space(d.day)
-        else
-          print two_space(d.day)
-        end
-      end
+      print ' '
     end
   end
 end
@@ -69,20 +55,6 @@ def space(d)
 end
 
 #土曜日ならtrueを返す
-def Sat?(d)
+def sat?(d)
   d == 6
 end
-
-#一文字スペースを作る
-def one_space(d)
-   " #{d} "
-end
-
-#2文字スペースを作る
-def two_space(d)
-   "  #{d} "
-end
-
-
-
-puts make_calendar(2019, 7)
