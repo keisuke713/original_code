ec9d45378f73b2bcf70f1c7a3242d9fc826a9005
keisuke713 <nebakei.tkb713@gmail.com>
2019-05-08 07:21:08 +0900

    add test

---
+++ test/runner_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require './lib/runner'
+
+class RunnerTest < Minitest::Test
+  def setup
+    @user1 = Runner.new('yamada', 9)
+    @user2 = Runner.new('katou', 10)
+  end
+
+  def test_runner
+    assert_equal 'yamadaの方が速い', @user1.competition(@user2)
+    assert_equal 'katouの方が遅い', @user2.competition(@user1)
+  end
+end
