146ea6043b1bae73632f1ce1a8418966295fcba9
keisuke713 <nebakei.tkb713@gmail.com>
2019-05-07 21:24:31 +0900

    add explain

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,4 +1,7 @@
+# 短距離走のタイムを競うプログラミングを作りました。
+
 class Runner
+  # timeは短距離走のタイム。
   attr_reader :name,:time
 
   def initialize(name,time)
@@ -10,7 +13,7 @@ class Runner
     return "#{self.name}の方が速い" if self > other
     "#{self.name}の方が遅い"
   end
-
+  # 短距離走ではタイムは小さい方が速いので>メソッドを再定義しました。
   def >(other)
     if other.is_a?(Runner)
       self.time < other.time
