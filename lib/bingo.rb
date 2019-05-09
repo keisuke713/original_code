b613300cdffbc7282cb3b1d0a11d92f6746c18ff
keisuke713 <nebakei.tkb713@gmail.com>
2019-05-09 21:10:11 +0900

    refactoring bingo

--- lib/bingo.rb
+++ lib/bingo.rb
@@ -1,24 +1,23 @@
 # ビンゴを作成しました。
 # 左の列から1~15,16~30,31~45,46~60,61~75からランダムに抽出し、真ん中は空白にします。
-RANGE_NUM = [
-  ('1'..'15'), ('16'..'30'), ('31'..'45'), ('46'..'60'), ('61'..'75')
-]
-
 class Bingo
-  def generate_card
+  attr_reader :numbers
+
+  def initialize
+    @numbers =
+      (1..75).each_slice(15).map { |array|
+        array.sample(5)
+      }
+    @numbers[2][2] = ''
+  end
+
+  def to_s
     puts %q!  B|  I|  N|  G|  O|!
-    # 配列の各要素からランダムに抽出する挙動を5回繰り返します。
-    # 3列目、3行目(真ん中)の時は空白にします。
-    1.upto(5) { |n|
-      RANGE_NUM.each_with_index { |range, index|
-        next print choose_num(range) unless n == 3 && index == 2
-        print '|'.rjust(4)
+    @numbers.transpose.each { |array|
+      array.each { |n|
+        print n.to_s.rjust(3) + '|'
       }
       puts
     }
-  end
-
-  def choose_num(range)
-    (range).to_a.sample.rjust(3) + '|'
   end
 end
--- lib/refactoring.rb
+++ lib/refactoring.rb
@@ -1,35 +1,5 @@
-RANGE_NUM.each_with_index { |i, index|
-  if n == 3 && index == 2
-    "%4d" % 1
-  else
-    print choose_num(i)
-  end
+array = [[1,2],[3,4]]
+
+array.each { |n|
+  p n
 }
-
-1.upto(5) { |n|
-  RANGE_NUM.each do |i|
-    print choose_num(i)
-  end
-  puts n
-}
-
-2.times do
-  RANGE_NUM.each do |i|
-    print choose_num(i)
-  end
-  puts
-end
-
-print choose_num('1'..'15')
-print choose_num('16'..'30')
-print '|'.rjust(4)
-print choose_num('46'..'60')
-print choose_num('61'..'75')
-puts
-
-2.times do
-  RANGE_NUM.each do |i|
-    print choose_num(i)
-  end
-  puts
-end
