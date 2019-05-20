3a3dc16f7888fe85a4ca1a76d2c03e806e3736dd
keisuke713 <nebakei.tkb713@gmail.com>
2019-05-20 21:14:07 +0900

    cherry_cal

---
+++ test/cherry_cal_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require './lib/cherry_calc'
+
+class CherryCalcTest < Minitest::Test
+  def test_cherry_calc
+    assert_equal '1, 9, 0', cherry_calc(1, 9)
+    assert_equal '5, 5, 4', cherry_calc(5, 9)
+  end
+end
