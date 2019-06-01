2e0b1f19c4d93f619e0d718c925c7ee95eefb1a4
keisuke713 <nebakei.tkb713@gmail.com>
2019-06-01 11:31:49 +0900

    rename variable and rewrite code

--- lib/log_filter.rb
+++ lib/log_filter.rb
@@ -1,15 +1,7 @@
 # 渡されたログから指定された文字列を含むログのみ抽出する
 #例 指定する文字列'ub',調べる文字列['ruby', 'php'] => ruby
 
-def log_filter(str,chars)
-  i = 0
-  while i < chars.length
-    include_chars = chars.select { |s|
-      s.include?(str)
-    }
-    i += 1
-  end
-
-  return include_chars if include_chars.length != 0
-  'None'
+def log_filter(included_char,chars)
+  extract_chars = chars.select { |char| char.include?(included_char) }
+  extract_chars.size > 0 ? extract_chars : 'None'
 end
