85efd9959324e54dc179f4452e3affbd6a75c0b2
keisuke713 <nebakei.tkb713@gmail.com>
2019-06-14 07:14:08 +0900

    create_array_code

---
+++ array.rb
@@ -0,0 +1,23 @@
+class Array
+
+  def initialize
+    @loop_count = 0
+  end
+
+  def alias_each
+    while @loop_count < self.size
+      value = yield self[@loop_count]
+      @loop_count += 1
+    end
+  end
+end
+
+array = Array.new.push(1,2,3,4,5)
+array.alias_each do |value|
+  p value * 2
+end
+
+array = Array.new.push('john', 'bob', 'michal')
+array.alias_each do |value|
+  p value.upcase
+end
